84456bcd48d36713953c96694f91e6349994e78f
Tavish <tavishgandhi4@gmail.com>
2021-06-01 13:22:58 +0530

    making devops project

---
+++ src/test/java/com/test/testproject/controller/AdditionControllerTest.java
@@ -0,0 +1,12 @@
+package com.test.testproject.controller;
+
+import junit.framework.TestCase;
+
+public class AdditionControllerTest extends TestCase {
+
+	public void testAddNumber() {
+		//fail("Not yet implemented");
+		System.out.println("This is Junit Test");
+	}
+
+}
---
+++ testproject1/target/testproject1/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<form action = "add">
+	<input type = "number" name = "t1">
+	<input type = "number" name = "t2">
+	<button type = "submit">Submit</button>
+</form>
+</body>
+</html>
